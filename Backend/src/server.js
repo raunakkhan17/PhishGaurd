4e7b97b464a6fbb7ce621fa6f12f4722bc11dd97
RAUNAK KHAN <raunak.khan4786@gmail.com>
2026-08-19 02:46:05 +0530

    5th commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -37,7 +37,13 @@ const allowedHeaders = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (
+      !origin ||
+      allowedOrigins.includes(origin) ||
+      origin.startsWith('chrome-extension://') ||
+      origin.startsWith('http://localhost') ||
+      origin.startsWith('http://127.0.0.1')
+    ) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
